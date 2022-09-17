5de8330b273121d60418fdac93a51260e829e33c
harold alzate <haroldalzate99@gmail.com>
2022-09-16 19:17:21 -0500

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'herous';
+  title:string = 'Tour of Heroes';
+  num:number = 0;
+  incrementar(){ 
+    this.num++;
+   }
 }
